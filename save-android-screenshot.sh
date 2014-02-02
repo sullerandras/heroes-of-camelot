91f7c3b06d9167150e436d8f99749fadfe936d72
Andras Suller <suller.andras@gmail.com>
2014-02-02 10:15:01 +0700

    Create 400x300 pixels screenshot

--- save-android-screenshot.sh
+++ save-android-screenshot.sh
@@ -9,7 +9,7 @@ ANDROID_SDK=~/android-sdks
 # echo "Taking screenshot via adb from connected Android device…"
 dateAndTime=$(date +"%Y-%m-%d at %H.%M.%S")
 $ANDROID_SDK/platform-tools/adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > temp.png
-convert -rotate -90 -crop 1400x1050+260+50 -resize 800 temp.png "$filename"
+convert -rotate -90 -crop 1400x1050+260+50 -resize 400 temp.png "$filename"
 
 # echo "Successfully saved screenshot to $filename"
 exit 0
