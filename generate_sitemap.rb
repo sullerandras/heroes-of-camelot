baacc2ab467b40d7e8bc0920ed032491ffb995e7
Andras Suller <suller.andras@gmail.com>
2014-06-22 23:03:57 +0800

    Update sitemap and 2 cards

--- generate_sitemap.rb
+++ generate_sitemap.rb
@@ -1,6 +1,6 @@
 BASE_URL = 'http://www.heroes-of-camelot.info'
 def generate_sitemap
-  cards_skills = (Dir['cards/*'] + Dir['skills/*']).map do |f|
+  cards_skills = (Dir['cards/*'] + Dir['combos/*']).map do |f|
     unless /-[1-4].jpg/.match f
       loc = "#{ BASE_URL }/#{ f.sub("s/", "/").sub(".jpg", "").gsub(" ", "%20") }"
       lastmod = File.mtime(f).strftime('%Y-%m-%d')
